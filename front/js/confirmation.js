981f2f1bce20302877cef46d5f27d75786ce5c5f
Pascal Roland <Pascal2601@live.fr>
2022-03-21 09:19:29 +0100

    ajout de confirmation.js et suppression des articles dans le cache

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,9 +3,3 @@ const idOrder = idSearchParams.get('orderId');
 
 const orderId = document.getElementById('orderId');
 orderId.innerHTML = `</br></br>${idOrder}`;
-
-removeAllCache();
-function removeAllCache() {
-  const cache = window.localStorage;
-  cache.clear();
-}
